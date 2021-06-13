8afbda5be2afad0422a84ce6e4e65f5d5382206b
AkhouriPalak <68817610+AkhouriPalak@users.noreply.github.com>
2021-06-14 01:23:58 +0530

    Update App.js

--- nFoodHome/App.js
+++ nFoodHome/App.js
@@ -4,6 +4,7 @@ import {
     Text,
     Image,
     TouchableOpacity,
+    StyleSheet,
     ScrollView,
 } from 'react-native';
 import unsplash from './Assets/unsplash.jpg';
@@ -12,10 +13,16 @@ const App = ()=>{
     return(
         <ScrollView>
         <View>
-            <Image source = {unsplash} ></Image>
+            <Image source = {unsplash} styles = {styles.container} ></Image>
         <Text></Text>
         </View>
         </ScrollView>
     )
 }
 export default App;
+
+const styles = StyleSheet.create({
+    container : {
+        flex:1,
+    }
+})
