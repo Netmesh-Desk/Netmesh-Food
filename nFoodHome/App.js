17a86c917945baed284150b8f63210badeadc4b9
AkhouriPalak <68817610+AkhouriPalak@users.noreply.github.com>
2021-06-14 01:10:02 +0530

    updated

--- nFoodHome/App.js
+++ nFoodHome/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {
     View,
     Text,
+    Image,
     TouchableOpacity,
 } from 'react-native';
+import unsplash from './Assets/unsplash.jpg';
 
 const App = ()=>{
     return(
         <>
         <View>
+            <Image ></Image>
         <Text></Text>
         </View>
         </>
