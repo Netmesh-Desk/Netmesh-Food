bde94ed7a715d18459f828fff86115ba54097e83
sourabhrathour45 <rathoursourabh5@gmail.com>
2021-06-13 21:30:50 +0530

    Update App.js

--- nFoodHome/App.js
+++ nFoodHome/App.js
@@ -1,17 +1,1 @@
 
-
-import React from 'react';
-
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-
-
-export default App;
