3e700ced6ab1ca7c4da54bf4bc64c8b911f688ad
AkhouriPalak <68817610+AkhouriPalak@users.noreply.github.com>
2021-06-14 01:46:00 +0530

    Update App.js

--- nFoodHome/App.js
+++ nFoodHome/App.js
@@ -12,18 +12,8 @@ import unsplash from './Assets/unsplash.jpg';
 const App = ()=>{
     return(
 
-        <ScrollView>
-        
-        
-     
-        <Image source = {unsplash} style = {styles.image1} ></Image>
-
-
-        <View>
-       
-
-        </View>
-        
+        <ScrollView  style = {styles.container}>
+       <Image source = {unsplash} style = {styles.image1} ></Image>
         </ScrollView>
     )
 }
