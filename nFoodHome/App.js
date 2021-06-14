891f82e77b2b52d81c29719b13da48d039a09305
AkhouriPalak <68817610+AkhouriPalak@users.noreply.github.com>
2021-06-14 22:05:26 +0530

    updated

--- nFoodHome/App.js
+++ nFoodHome/App.js
@@ -9,20 +9,21 @@ import {
     ImageBackground,
 } from 'react-native';
 import unsplash from './Assets/unsplash.jpg';
+import logo from './Assets/logo.jpeg';
 
 const App = ()=>{
     return(
-        <ScrollView>
         <View style = {styles.container}>
-     
-       
-        {/* <ImageBackground source = {unsplash} style = {styles.image1} ></ImageBackground>
-  */}
-        <Text>hello</Text>
-        </View>
+            <View style = {styles.textContainer}><Text style = {styles.text}>N Food              
+            <Image source = {logo} style = {styles.imageLogo}></Image></Text>
+            </View>
+        <ImageBackground source = {unsplash} style = {styles.image1} >
+        <ScrollView>
         </ScrollView>
+        </ImageBackground>
        
-        
+        </View>
+      
     )
 }
 export default App;
@@ -39,5 +40,26 @@ const styles = StyleSheet.create({
         width: 500,
         opacity : 0.4,
     },
+    text : {
+        fontSize : 28,
+        color : "#000000",
+        fontWeight : "bold",
+        fontFamily: "Roboto",
+        paddingTop:8,
+        paddingLeft :15,
+    },
+    textContainer : {
+        height : 50,
+        width : 500,
+        backgroundColor : "#FFF",
+        
+    },
+    imageLogo : {
+        width :30,
+        height:30,
+        paddingLeft :80,
+        justifyContent : "flex-end",
+        alignItems:"flex-end",
+    }
 
 })
