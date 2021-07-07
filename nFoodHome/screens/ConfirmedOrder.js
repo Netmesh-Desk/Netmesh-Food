62a6a240ec4b7106a1b8aeb95ded13efb6f5068f
sourabhrathour45 <rathoursourabh5@gmail.com>
2021-07-07 09:12:06 +0530

    firebase

--- nFoodHome/screens/ConfirmedOrder.js
+++ nFoodHome/screens/ConfirmedOrder.js
@@ -1,16 +1,33 @@
-import React  from 'react';
+import React , {useState} from 'react';
 import {Text, View, StyleSheet, Image, TextInput, ScrollView, TouchableOpacity} from 'react-native';
 import { widthToDp, heightToDp } from './dimension';
 
 const ConfirmedOrder = ({navigation})=>{
+  
+
+
+
+
+
     return(
         <>
         <ScrollView keyboardShouldPersistTaps = "handled" backgroundColor = "#101010" style = {{flex : 1 }}>
         <Text style = {{color :"#FFF", fontSize : 35, fontWeight : "bold", marginLeft: 26, marginTop: 55}}>Create Account,</Text>
         <Text style = {{color :"#E1E1E1", fontSize : 15, marginLeft: 28}}>Sign Up to get started!</Text>
-         <TextInput placeholder = "Name" placeholderTextColor= "#D1D1D1" keyboardType = "name-phone-pad" style = {{color : "#FFF", width : 290 , height : 40 , borderWidth : 2 , borderColor : "#19480B" , marginTop : 100,  alignSelf : "center", borderRadius : 16, paddingLeft : 20, paddingTop : 10}}></TextInput>
-         <TextInput placeholder = "Email" placeholderTextColor= "#D1D1D1" keyboardType = "email-address" style = {{color : "#FFF", width : 290 , height : 40 , borderWidth : 2 , borderColor : "#19480B" , marginTop : 40,  alignSelf : "center", borderRadius : 16, paddingLeft : 20, paddingTop : 10}}></TextInput>
-         <TextInput placeholder = "Password" placeholderTextColor= "#D1D1D1" keyboardType = "visible-password" secureTextEntry = {true} style = {{color : "#FFF", width : 290 , height : 40 , borderWidth : 2 , borderColor : "#19480B" , marginTop : 40,  alignSelf : "center", borderRadius : 16, paddingLeft : 20, paddingTop : 10}}></TextInput>
+
+
+         <TextInput placeholder = "Name" placeholderTextColor= "#D1D1D1" keyboardType = "name-phone-pad" 
+         style = {{color : "#FFF", width : 290 , height : 40 , borderWidth : 2 , borderColor : "#19480B" , marginTop : 100,  alignSelf : "center", borderRadius : 16, paddingLeft : 20, paddingTop : 10}}></TextInput>
+
+
+         <TextInput placeholder = "Email" placeholderTextColor= "#D1D1D1" keyboardType = "email-address" 
+         style = {{color : "#FFF", width : 290 , height : 40 , borderWidth : 2 , borderColor : "#19480B" , marginTop : 40,  alignSelf : "center", borderRadius : 16, paddingLeft : 20, paddingTop : 10}}></TextInput>
+
+
+         <TextInput placeholder = "Password" placeholderTextColor= "#D1D1D1" keyboardType = "visible-password" secureTextEntry = {true} 
+         style = {{color : "#FFF", width : 290 , height : 40 , borderWidth : 2 , borderColor : "#19480B" , marginTop : 40,  alignSelf : "center", borderRadius : 16, paddingLeft : 20, paddingTop : 10}}></TextInput>
+
+
          <TouchableOpacity activeOpacity = {0.5}   onPress = {() => navigation.navigate("Home")} >
          <Text style = {{  fontSize : widthToDp(4.5) , height : 47 , width : widthToDp(29) , color : "#FFF", borderWidth : 1 , borderColor :"#1FAA08", backgroundColor : "#0B4801" ,
          alignSelf :"center", paddingTop : 11, paddingLeft : 18, borderRadius : 9, fontWeight : "bold", marginTop : 120}}> Sign Up</Text> 
--- nFoodHome/screens/loginScreenUI.js
+++ nFoodHome/screens/loginScreenUI.js
@@ -1,16 +1,26 @@
-import React  from 'react';
+import React , {useState} from 'react';
 import {Text, View, StyleSheet, Image, TextInput, ScrollView, TouchableOpacity} from 'react-native';
 import { widthToDp, heightToDp } from './dimension';
 
+
 const ConfirmedOrder = ({navigation})=>{
+    
+  
+
     return(
         <>
         <ScrollView keyboardShouldPersistTaps = "handled" backgroundColor = "#101010" style = {{flex : 1 }}>
         <Text style = {{color :"#FFF", fontSize : 40, fontWeight : "bold", marginLeft: 26, marginTop: 65}}>Welcome,</Text>
         <Text style = {{color :"#E1E1E1", fontSize : 15, marginLeft: 32}}>Sign In to Continue!</Text>
         
-         <TextInput placeholder = "Email" placeholderTextColor= "#D1D1D1" keyboardType = "email-address" style = {{color : "#FFF", width : 290 , height : 40 , borderWidth : 2 , borderColor : "#19480B" , marginTop : 110,  alignSelf : "center", borderRadius : 16, paddingLeft : 20, paddingTop : 10}}></TextInput>
-         <TextInput placeholder = "Password" placeholderTextColor= "#D1D1D1" keyboardType = "visible-password"  secureTextEntry = {true} style = {{color : "#FFF", width : 290 , height : 40 , borderWidth : 2 , borderColor : "#19480B" , marginTop : 40,  alignSelf : "center", borderRadius : 16, paddingLeft : 20, paddingTop : 10}}></TextInput>
+         <TextInput placeholder = "Email" placeholderTextColor= "#D1D1D1" keyboardType = "email-address"
+         style = {{color : "#FFF", width : 290 , height : 40 , borderWidth : 2 , borderColor : "#19480B" , marginTop : 110,  alignSelf : "center", borderRadius : 16, paddingLeft : 20, paddingTop : 10}}></TextInput>
+
+
+         <TextInput placeholder = "Password" placeholderTextColor= "#D1D1D1" keyboardType = "visible-password"  secureTextEntry = {true} 
+         style = {{color : "#FFF", width : 290 , height : 40 , borderWidth : 2 , borderColor : "#19480B" , marginTop : 40,  alignSelf : "center", borderRadius : 16, paddingLeft : 20, paddingTop : 10}}></TextInput>
+
+
          <TouchableOpacity activeOpacity = {0.5}   onPress = {() => navigation.navigate("Home")} >
          <Text style = {{  fontSize : widthToDp(4.5) , height : 47 , width : widthToDp(29) , color : "#FFF", borderWidth : 1 , borderColor :"#1FAA08", backgroundColor : "#0B4801" ,
          alignSelf :"center", paddingTop : 11, paddingLeft : 20, borderRadius : 9, fontWeight : "bold", marginTop : 120}}> Sign In</Text> 
